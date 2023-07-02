f116309badd17c2d102bc6fb7ddd3df869bd584f
6ixFeetWinter <104249266+6ixFeetWinter@users.noreply.github.com>
2023-07-02 21:42:13 +0900

    こちらお願いします

---
+++ todo-app/src/components/List.tsx
@@ -0,0 +1,166 @@
+import React, { useState, Dispatch, SetStateAction } from "react";
+import { useTodoList } from "../hooks/TodoList";
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ClearIcon from "@mui/icons-material/Clear";
+import { Select, TextField } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import { pink } from "@mui/material/colors";
+
+type Todo = {
+  title: string;
+  state: boolean;
+};
+type ListProps = {
+  todoList: Array<Todo>;
+  setTodoList: Dispatch<SetStateAction<Todo[]>>;
+};
+const listItem = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 10px;
+  width: 800px;
+  height: 100px;
+  margin: 10px auto;
+  padding: 0 10px;
+  border-radius: 10px;
+`;
+const buttonArea = css`
+  display: flex;
+  gap: 20px;
+  list-style-type: none;
+`;
+const selectorStyle = css`
+  border: none;
+  border-radius: 3px;
+  background-color: white;
+  color: gray;
+  border-radius: 10%;
+`;
+const editArea = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+export const List: React.FC<ListProps> = ({ todoList, setTodoList }) => {
+  const [edit, setEdit] = useState<number | null>(null);
+  const [changeText, setChangeText] = useState("");
+  const [changeState, setChangeState] = useState(false);
+  const stateCheck = (index: number) => {
+    const newList = todoList.map((prev, prevIndex) => {
+      if (prevIndex === index) {
+        return {
+          title: changeText,
+          state: false,
+        };
+      }
+      return prev;
+    });
+    setTodoList([...newList]);
+    setEdit(null);
+    setChangeText("");
+    localStorage.setItem("key", JSON.stringify(todoList));
+  };
+  const onDelete = (index: number) => {
+    const newList = [...todoList];
+    newList.splice(index, 1);
+    setTodoList([...newList]);
+    localStorage.setItem("key", JSON.stringify(todoList));
+  };
+  const stateUpdate = (e: any, index: number) => {
+    const newChangeState = e.target.value === "completed";
+    setChangeState(newChangeState);
+    const newList = todoList.map((prev, prevIndex) => {
+      if (prevIndex === index) {
+        return {
+          title: prev.title,
+          state: newChangeState,
+        };
+      }
+      return prev;
+    });
+    setTodoList([...newList]);
+    setEdit(null);
+    localStorage.setItem("key", JSON.stringify(todoList));
+  };
+  return (
+    <ul>
+      {todoList.map((todo, index) => (
+        <li
+          key={index}
+          css={listItem}
+          style={
+            todo.state
+              ? { backgroundColor: "#0164C9", opacity: ".5" }
+              : { backgroundColor: "#D1DAFE", color: "gray" }
+          }
+        >
+          <label htmlFor="state-selector">
+            <Select
+              defaultValue={"undone"}
+              name="state"
+              id="state-selector"
+              css={selectorStyle}
+              onChange={(e) => stateUpdate(e, index)}
+            >
+              <MenuItem value={"undone"}>未達成</MenuItem>
+              <MenuItem value={"completed"}>達成</MenuItem>
+            </Select>
+          </label>
+          <p style={{ fontSize: "24px" }}>{todo.title}</p>
+          <div>
+            {edit === index ? (
+              <div css={editArea}>
+                <TextField
+                  type="text"
+                  value={changeText}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setChangeText(e.target.value)
+                  }
+                />
+                <button
+                  style={{ border: "none", backgroundColor: "#0DC809" }}
+                  onClick={() => stateCheck(index)}
+                >
+                  <EditIcon />
+                </button>
+                <button
+                  style={{ border: "none", backgroundColor: "#AE1313" }}
+                  onClick={() => setEdit(null)}
+                >
+                  <ClearIcon />
+                </button>
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+          <ul css={buttonArea}>
+            <li>
+              <button
+                style={{ border: "none", backgroundColor: "#0DC809" }}
+                type="button"
+                onClick={() => setEdit(index)}
+              >
+                <EditIcon />
+              </button>
+            </li>
+            <li>
+              <button
+                style={{ border: "none", backgroundColor: "#AE1313" }}
+                type="button"
+                onClick={() => onDelete(index)}
+              >
+                <DeleteForeverIcon />
+              </button>
+            </li>
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
+};
